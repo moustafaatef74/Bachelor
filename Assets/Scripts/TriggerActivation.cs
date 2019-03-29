a25c01e8705227dd08b1009b748e3fef1a8f5c80
moustafaatef74 <moustafaatef74@gmail.com>
2019-03-29 20:01:05 +0200

    29/3

---
+++ Assets/Scripts/TriggerActivation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TriggerActivation : MonoBehaviour
+{
+    public GameObject Spot;
+    public GameObject teleportingPlane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (this.transform.name == "TurnOffLight")
+        {
+            Spot.gameObject.GetComponent<Light>().intensity = 0;
+            teleportingPlane.SetActive(true);
+            
+        }
+        if(this.transform.name == "SolvingScenePortal")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
